e981112431d025cf55edbb4dc4c0a4ccd0595763
Juan Carlos <krlos013@gmail.com>
2021-06-29 22:31:05 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+import './index.css';
 import Contact from './pages/Contact'
 
 function App() {
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,57 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import styled from 'styled-components'
 const Contact = () => {
+    const [description, setDescription] = useState()
+    const [phone, setPhone] = useState()
+    const [name, setName] = useState()
+    const [color, setColor] = useState("bg-yellow-400 items-center hover:bg-yellow-400  text-center flex justify-center font-bold")
+    const [formulario, setFormulario] = useState("grid grid-flow-col grid-cols-1 grid-rows-1 gap-4")
+    const [thanks, setThanks] = useState("hidden")
+    const [boton, setBoton] = useState("flex w-full justify-center  mt-10")
+    const [label, setLabel] = useState("SEND")
+
+    const send = () =>{
+        if (description && phone && name) {
+            const contenido = {
+                "content": "Hola, me interesa realizar un proyecto con ustedes!",
+                "embeds": [{
+                    "title": phone,
+                    "description": description,
+                    "footer": {
+                        "text": "Gracias, atte:" + " " + name
+                    }
+                }
+                ]
+            }
+
+            fetch('https://discordapp.com/api/webhooks/859145497657475082/isvUHnFPxe0-_I1sArQyn3MmB0s2wLQ_TxYM4Li_w0ZAbSubQlOgD3xsr2ok6s6nF2R1', {
+                method: 'POST',
+                body: JSON.stringify(contenido),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(res => {
+                    setDescription("")
+                    setName("")
+                    setPhone("")
+                    setFormulario("hidden grid grid-flow-col grid-cols-3 grid-rows-1 gap-4")
+                    setThanks("grid grid-flow-col grid-cols-3 grid-rows-1 gap-4")
+                    setBoton("hidden")
+                })
+
+        }
+        else {
+            setColor("text-white items-center bg-red-500 text-center flex justify-center font-bold hover:bg-red-400")
+            setLabel("Error try again")
+        }
+
+    }
     return (
-        <div className="flex flex-row justify-center">
+        <div className="h-screen bg-black flex flex-col justify-center p-60">
                                 <div className="text-center h-auto">
                                     <div className="mb-5 text-yellow-400 text-xl font-semibold ">TELL US ABOUT YOUR IDEAS</div>
-                                    
                                     <div className={formulario}>
-                                        
                                         <div className="flex flex-col">
                                             <div className="flex flex-col">
                                                 <label className="text-white text-base font-normal p-1">Describe your idea.</label>
@@ -21,7 +65,7 @@ const Contact = () => {
                                     </div>
                                     <div className={thanks}>
                                         <div></div>
-                                        <Typed strings={['THANK YOU']} typeSpeed={90} className="text-center text-4xl font-bold text-gray-100 mt-10 mb-10"></Typed>
+                                        <div className="text-center text-4xl font-bold text-gray-100 mt-10 mb-10">THANK YOU</div>
                                         <div></div>
                                     </div>
                 </div>
@@ -33,3 +77,11 @@ const Contact = () => {
 };
 
 export default Contact;
+
+
+const Boton = styled.button`
+    padding:5px;
+    height:60px;
+    width: 150px;
+    border-radius: 50px;
+`
